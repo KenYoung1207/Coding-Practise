4ac943f05f6b7649c9d3b7d26a9961ad339084e8
KenYoung1207 <109553460+KenYoung1207@users.noreply.github.com>
2022-07-31 15:27:30 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,3 +4,19 @@
 import pandas as pd
 import numpy as np
 
+## EXAMPLE 1
+basket_items = {'apples': 4, 'oranges':19, 'kites':3, 'sandwiches':8}
+fruits = ['apples','oranges','pears','peaches','grapes']
+
+# Create a local variable total 
+# Specify value of "total" = 0
+total = 0
+
+# Iterate through basket_items
+# For each iteration, check if index matches "fruits"
+# If it matches, add the value into "total"
+for index, value in basket.items():
+  if index in fruits:
+    total += value
+
+print (total)
